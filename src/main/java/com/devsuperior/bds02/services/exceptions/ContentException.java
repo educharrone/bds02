2b2f325cdec262311310acc72170c65c838b7529
educharrone@hotmail.com <educharrone@hotmail.com>
2021-07-15 01:02:37 -0300

    Add project

---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/ContentException.java
@@ -0,0 +1,12 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class ContentException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L; 
+
+	public ContentException(String msg) {
+		
+		super(msg);
+	}
+	
+}
